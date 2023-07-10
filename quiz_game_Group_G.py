46f3879c173080c5b6064584f2269d0e464e3174
OscarZhong96 <138770114+OscarZhong96@users.noreply.github.com>
2023-07-10 16:53:00 +0100

    Update quiz_game_Group_G.py

--- quiz_game_Group_G.py
+++ quiz_game_Group_G.py
@@ -1,1 +1,35 @@
+intro="Welcome to the Group G pub quiz"
+print(intro.center(60,"*"))
+name=input("\nInput your name here:")
+print("Welcome to the Millionaire Game" ,name)
 
+ques = ("\nQ1. Which character is used in Python to make a single line comment? \nA./ \nB.// \nC.Hash \nD.!", 
+        "\nQ2. What is the Capital of INDIA? \nA. Mumbai \nB.Goa \nC.New Delhi \nD.Chennai",
+        "\nQ3. What is the Mother Tongue of Maharashtra State? \nA.Marathi \nB.Telugu \nC.Gujrati \nD.Bengoli", 
+        "\nQ4. Most IPL trophies Won by which team? \nA.MumbaiIndians \nB.RoyalChallengerBanglore \nC.ChennaiSuperKings \nD.KolkattaKnightRiders", 
+        "\nQ5. Current Most popular programming language in the world? \nA.Java \nB.C++ \nC.JavaScript \nD.Python")
+
+ans = (("C" or "c"), ("C" or "c"), ("A" or "a"), ("A" or"a"),("D" or "d"))
+prize=(100,200,300,400,500)
+startingAmount=0
+
+for i in range(0, len(ques)):
+  print(ques[i])
+
+  Ans = input("Select your Answer OR to Quit press 'Q' : ")
+  if (Ans == "Q"):
+    print("Thank You for Playing!")
+    break
+  if Ans == ans[i]:
+    print("Correct ! \nYou Won the pricepool of £.",prize[i])
+    startingAmount=prize[i]
+  else : 
+   print("Incorrect ! \nYou lose the Game.")
+   break
+
+print("\n\nTotal Amount Won by",name,"is £",startingAmount)
+# if(i==2):
+#   prize=2000
+# elif (i==3):
+#   prize=3000
+print(".....Thank You !! For Prticipating in Millionaire Game.....") 
